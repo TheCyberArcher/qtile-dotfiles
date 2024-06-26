d08b303456c8b82ca2a5e4ddf77c81a99486d4bc
If cli is a gift from god, using arch makes us prophets † <62696671+TheCyberArcher@users.noreply.github.com>
2024-06-26 21:11:41 +0000

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -3,10 +3,14 @@
 
 #System
 
-xrandr --output DP-2 --mode 3440x1440 --rate 144
+
+xrandr --output DP-1 --primary --mode 3440x1440 --rate 144 --pos 1440x474 --rotate normal --output DP-2 --off --output DP-3 --off &
+
+xwinwrap -ni -b -fs -ov  -- mpv -wid WID --loop --no-audio /home/alerion/Vidéos/sadgirl.mp4 &
+
 picom &
 lxpolkit &
-nm-applet &
+nm-applet &z
 exec /usr/lib/geoclue-2.0/demos/agent &
 
 #applications :
@@ -17,6 +21,7 @@ discord --start-minimized &
 flameshot &
 redshift-gtk &
 signal-desktop -- %u --use-tray-icon --start-in-tray --no-sandbox %U &
+solaar --window=hide &
 
 #Flatpak :
 
