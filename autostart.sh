6418819b8e856f82e258a65796859dc9dc8579e4
If cli is a gift from god, using arch makes us prophets † <62696671+TheCyberArcher@users.noreply.github.com>
2024-07-14 09:18:04 +0000

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -3,28 +3,23 @@
 
 #System
 
-
 xrandr --output DP-1 --primary --mode 3440x1440 --rate 144 --pos 1440x474 --rotate normal --output DP-2 --off --output DP-3 --off &
 
-xwinwrap -ni -b -fs -ov  -- mpv -wid WID --loop --no-audio /home/alerion/Vidéos/sadgirl.mp4 &
+xwinwrap -ni -b -fs -ov  -- mpv -wid WID --loop --no-audio --vf-add=fps=60 --hwdec=auto-copy /home/alerion/Vidéos/sadgirl.mp4&
 
 picom &
-lxpolkit &
-nm-applet &z
-exec /usr/lib/geoclue-2.0/demos/agent &
+nm-applet &
+plank &
 
 #applications :
 
-corectrl &
+corectrl --minimize-systray &
 protonvpn-app &
-discord --start-minimized &
 flameshot &
 redshift-gtk &
 signal-desktop -- %u --use-tray-icon --start-in-tray --no-sandbox %U &
+discord --start-minimized &
 solaar --window=hide &
-
-#Flatpak :
+protonmail-bridge --no-window &
 
 flatpak run --branch=stable --arch=x86_64 --command=whatsapp-desktop-linux io.github.mimbrero.WhatsAppDesktop --start-hidden &
-
-dex -a
